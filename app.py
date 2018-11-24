b2737de95bc44a5794c46a54e6849136177ab03a
Lucas Werkmeister <mail@lucaswerkmeister.de>
2018-11-24 09:01:20 +0100

    Implement route for lexemes by a user (#13)
    * Add hidden language input to lemma.html page
    
    This means that POSTs to this page can be processed independently of the
    route, which will be useful for #11.
    
    * Set language on input
    
    Some browsers enable spellchecking by default on single-line inputs, and
    the user might also have enabled it manually, so let’s make sure the
    browser checks spelling against the correct language.
    
    * Refactor add() function
    
    Extract the two main halves of add() into separate functions: one to
    display the page for a certain lexeme (dropping the unused `total`
    argument), one to handle a POST for it. We should be able to reuse both
    for #11, with a different “driver” function to select the lexeme.
    
    Also includes some other minor improvements: use flask.jsonify(), and
    fix CSRF error detection (had no effect previously, now the form will be
    repeated – with no special indication so far, but it’s something). Also,
    submit_lexeme() no longer returns anything – instead of issuing a
    redirect to the current URL (effectively asking the user to reload the
    page), just fall through to responding with the page directly. (Unless
    in “dev” mode, where the JSON is returned.)
    
    * Add route for lexemes by a user
    
    This adds another route, /user/<user_name>, to pick a random lexeme
    created by that user (typically your own, but you can also clean up
    after someone else, or a bot account) which doesn’t have a sense yet.
    The query to select the lexemes is adapted from T209034 [1].
    
    If the query returns no results, either due to T209034 (incomplete
    continuation) or because there really are no lexemes by that user
    without senses, then the result is currently a server error. Perhaps
    that can be improved later.
    
    The route is not advertised anywhere yet, but I plan to add a link to
    “your own lexemes” for logged-in users later.
    
    Implements #11.
    
    [1]: https://phabricator.wikimedia.org/T209034
    
    * Extract function to get and cache logged in user name
    
    For the index function, we’ll need this information more than once, so
    to avoid making two requests to the 'identify' API, cache the result in
    the application context [1]. (The application context is destroyed at
    the end of each request, so this should not result in stale information
    across requests.)
    
    [1]: http://flask.pocoo.org/docs/1.0/appcontext/
    
    * Add link to current user’s lexemes to index page
    
    Part of #11: when a user is logged in, show them a link to their own
    lexemes without senses.
    
    The phrasing and design is rubbish, I couldn’t think of anything better.
    Feel free to replace it. (Perhaps include the number of such lexemes in
    the description, and don’t show the link if it’s zero?)
    
    * Redirect user after submitting a sense
    
    If we don’t redirect the user after they’ve submitted a sense, and
    instead fall through to directly responding with another form, then an
    attempt to reload the page (typically intended to get another lexeme)
    will instead be interpreted by the browser as an attempt to submit the
    form again, which does not make sense.
    
    * Fix “give me another lemma” button target
    
    Make sure it always links to the current page (i. e. reloads it),
    instead of the 'add' endpoint, which is not appropriate when using the
    'user' endpoint.

--- app.py
+++ app.py
@@ -77,22 +77,36 @@ def user_link(user_name):
 
 
 @app.template_global()
-def authentication_area():
-    if 'oauth' not in app.config:
-        return flask.Markup()
+def logged_in_user_name():
+    if 'user_name' in flask.g:
+        return flask.g.user_name
 
+    if 'oauth' not in app.config:
+        return flask.g.setdefault('user_name', None)
     if 'oauth_access_token' not in flask.session:
-        return (flask.Markup(r'<a id="login" class="navbar-text" href="') +
-                flask.Markup.escape(flask.url_for('login')) +
-                flask.Markup(r'">Log in</a>'))
+        return flask.g.setdefault('user_name', None)
 
     access_token = mwoauth.AccessToken(**flask.session['oauth_access_token'])
     identity = mwoauth.identify('https://www.wikidata.org/w/index.php',
                                 consumer_token,
                                 access_token)
+    return flask.g.setdefault('user_name', identity['username'])
+
+
+@app.template_global()
+def authentication_area():
+    if 'oauth' not in app.config:
+        return flask.Markup()
+
+    user_name = logged_in_user_name()
+
+    if user_name is None:
+        return (flask.Markup(r'<a id="login" class="navbar-text" href="') +
+                flask.Markup.escape(flask.url_for('login')) +
+                flask.Markup(r'">Log in</a>'))
 
     return (flask.Markup(r'<span class="navbar-text">Logged in as ') +
-            user_link(identity['username']) +
+            user_link(user_name) +
             flask.Markup(r'</span>'))
 
 
@@ -144,13 +158,20 @@ def build_senses(form_data, lang):
     return sense_data
 
 
-def current_url():
-    return flask.url_for(
-        flask.request.endpoint,
-        _external=True,
-        _scheme=flask.request.headers.get('X-Forwarded-Proto', 'http'),
-        **flask.request.view_args
-    )
+@app.template_global()
+def current_url(external=True):
+    if external:
+        return flask.url_for(
+            flask.request.endpoint,
+            _external=True,
+            _scheme=flask.request.headers.get('X-Forwarded-Proto', 'http'),
+            **flask.request.view_args
+        )
+    else:
+        return flask.url_for(
+            flask.request.endpoint,
+            **flask.request.view_args
+        )
 
 
 def generate_auth():
@@ -181,7 +202,6 @@ def submit_lexeme(word_id, senses, lang):
         token=token,
         id=word_id
     )
-    return flask.redirect(flask.url_for('add', lang=lang))
 
 
 def get_with_missing_senses(lang):
@@ -205,37 +225,96 @@ def get_with_missing_senses(lang):
     return results["results"]["bindings"]
 
 
+def get_with_missing_senses_by_user(user_name):
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+    sparql.setQuery("""
+        SELECT ?lexeme ?languageCode ?lexicalCategoryLabel (GROUP_CONCAT(DISTINCT ?lemma; separator = "/") AS ?lemmas) WHERE {
+          hint:Query hint:optimizer "None".
+          SERVICE wikibase:mwapi {
+            bd:serviceParam wikibase:endpoint "www.wikidata.org";
+                            wikibase:api "Generator";
+                            mwapi:generator "allrevisions";
+                            mwapi:garvuser "%s";
+                            mwapi:garvnamespace "146";
+                            mwapi:garvend "2018-05-23T00:00:00.000Z";
+                            mwapi:garvlimit "max".
+            ?title wikibase:apiOutput mwapi:title.
+          }
+          BIND(URI(CONCAT(STR(wd:), STRAFTER(?title, "Lexeme:"))) AS ?lexeme)
+          MINUS { ?lexeme ontolex:sense ?sense. }
+          ?lexeme wikibase:lemma ?lemma;
+                  dct:language/wdt:P424 ?languageCode;
+                  wikibase:lexicalCategory ?lexicalCategory.
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+        }
+        GROUP BY ?lexeme ?languageCode ?lexicalCategoryLabel
+        """ % user_name.replace(' ', '_').replace('\\', '\\\\').replace('"', r'\"'))
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    return results["results"]["bindings"]
+
+
+def submit_sense_from_request():
+    token = flask.session.pop('csrf_token', None)
+    if not token or token != flask.request.form.get('csrf_token'):
+        flask.g.csrf_error = True
+        flask.g.repeat_form = True
+        return None
+
+    if 'oauth' in app.config:
+        form_data = flask.request.form
+        lang = form_data['lang']
+        senses = build_senses(form_data, lang)
+        word_id = form_data["word_id"]
+        submit_lexeme(word_id, senses, lang)
+        return None
+    else:
+        return flask.jsonify(senses)
+
+
+def show_lemma_page(lemma, lang, word_id, pos):
+    return flask.render_template('lemma.html',
+                                 lemma=lemma,
+                                 lang=lang,
+                                 word_id=word_id,
+                                 pos=pos,
+                                 csrf_error=flask.g.get('csrf_error', False))
+
+
 @app.route('/add/<lang>', methods=['GET', 'POST'])
 def add(lang):
+    if flask.request.method == 'POST':
+        response = submit_sense_from_request()
+        if response:
+            return response
+        else:
+            return flask.redirect(flask.url_for('add', lang=lang))
+
     words = get_with_missing_senses(lang)
     random_word = random.choice(words)
     random_word_id = random_word["l"]["value"].split("/")[-1]
-    csrf_error = False
+    return show_lemma_page(lemma=random_word["lemma"]["value"],
+                           lang=lang,
+                           word_id=random_word_id,
+                           pos=random_word["posLabel"]["value"])
+
+
+@app.route('/user/<user_name>', methods=['GET', 'POST'])
+def user(user_name):
     if flask.request.method == 'POST':
-        token = flask.session.pop('csrf_token', None)
-        if token and token == flask.request.form.get('csrf_token'):
-            flask.session['sense'] = flask.request.form.get(
-                'sense', 'sense missing')
+        response = submit_sense_from_request()
+        if response:
+            return response
         else:
-            csrf_error = True
-            flask.g.repeat_form = True
+            return flask.redirect(flask.url_for('user', user_name=user_name))
 
-        if 'oauth' in app.config:
-            form_data = flask.request.form
-            senses = build_senses(form_data, lang)
-            word_id = form_data["word_id"]
-            return submit_lexeme(word_id, senses, lang)
-        else:
-            return flask.Response(json.dumps(senses),
-                                  mimetype='application/json')
-
-    return flask.render_template('lemma.html',
-                                 lemma=random_word["lemma"]["value"],
-                                 lang=lang,
-                                 total=len(words),
-                                 word_id=random_word_id,
-                                 pos=random_word["posLabel"]["value"],
-                                 csrf_error=csrf_error)
+    words = get_with_missing_senses_by_user(user_name)
+    random_word = random.choice(words)
+    random_word_id = random_word["lexeme"]["value"].split("/")[-1]
+    return show_lemma_page(lemma=random_word["lemmas"]["value"],
+                           lang=random_word["languageCode"]["value"],
+                           word_id=random_word_id,
+                           pos=random_word["lexicalCategoryLabel"]["value"])
 
 
 @app.route('/login')
